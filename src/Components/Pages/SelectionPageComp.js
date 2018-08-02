5bb3b2d0a00168fb54abb935fa3b9b518e437ed7
amysudarat <amy.sudarat@gmail.com>
2018-08-02 12:34:36 -0400

    enable emoji selection -done

--- src/Components/Pages/SelectionPageComp.js
+++ src/Components/Pages/SelectionPageComp.js
@@ -21,16 +21,22 @@ export default class SelectionPageComp extends Component {
         <div className="container">
           <div className="row my-5">
             <div class="col-sm">
-              <EmojiComponent emotion="Happy" onClick={(e)=>this.update(e,"Happy")} />
+              <EmojiComponent alt="Very Happy" emotion="VeryHappy" onClick={(e)=>this.update(e,"VeryHappy")} />
             </div>
             <div className="col-sm">
-              <EmojiComponent emotion="Sad" onClick={(e)=>this.update(e,"Sad")} />
+              <EmojiComponent alt="Happy" emotion="Happy" onClick={(e)=>this.update(e,"Happy")} />
             </div>
             <div className="col-sm">
-              <EmojiComponent emotion="Angry" onClick={(e)=>this.update(e,"Angry")} />
+              <EmojiComponent alt="Neutral" emotion="Neutral" onClick={(e)=>this.update(e,"Neutral")} />
             </div>
             <div className="col-sm">              
-              <EmojiComponent emotion="Neutral" onClick={(e)=>this.update(e,"Neutral")}/>
+              <EmojiComponent alt="Sad" emotion="Sad" onClick={(e)=>this.update(e,"Sad")}/>
+            </div>
+            <div className="col-sm">              
+              <EmojiComponent alt="Very Sad" emotion="VerySad" onClick={(e)=>this.update(e,"VerySad")}/>
+            </div>
+            <div className="col-sm">              
+              <EmojiComponent alt="Angry" emotion="Angry" onClick={(e)=>this.update(e,"Angry")}/>
             </div>
           </div>
         </div>
@@ -39,7 +45,8 @@ export default class SelectionPageComp extends Component {
           <div className="row my-5 d-flex align-item-center">
             <div class="col-lg">
               <EmojiComponent 
-                emotion={this.state.currentEmotion} />
+                emotion={this.state.currentEmotion}
+                size = {100} />
             </div>
           </div>
         </div>

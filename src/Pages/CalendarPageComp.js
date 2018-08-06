4b05ea5cc0dfe09b7e5aa608ce007cfec4eb7fb6
Mauricio S <mauriciosantoshoyos@gmail.com>
2018-08-06 11:47:44 -0400

    add calendar with mock data

--- src/Pages/CalendarPageComp.js
+++ src/Pages/CalendarPageComp.js
@@ -1,13 +1,51 @@
 import React, {Component} from 'react';
+import Calendar from "react-big-calendar";
+import moment from "moment";
+import EmojiComponent from "../Components/EmojiComponent.js";
+
+import "react-big-calendar/lib/css/react-big-calendar.css";
+
+Calendar.setLocalizer(Calendar.momentLocalizer(moment));
+
+const data = [
+  {
+    start: "2018-08-01T15:36:27.906Z",
+    end: "2018-08-01T15:36:27.906Z", 
+    title: <EmojiComponent emotion="VerySad"/>
+  },
+  {
+    start: "2018-08-03T15:36:27.906Z",
+    end: "2018-08-03T15:36:27.906Z", 
+    title: <EmojiComponent emotion="VeryHappy"/>
+  },
+  {
+    start: new Date(),
+    end: new Date(),
+    title: <EmojiComponent emotion="VerySad"/>
+  },
+]
+
+
 
 export default class CalendarPageComp extends Component {
-    render() {
-        return (
-          <div className="my-5">
-            <h1 >Calendar Page</h1>
-    
-          </div>  
-        );
-    }
+  state = {
+      events: data
+  };
+
+  render() {
+      return (
+        <div className="my-5 container">
+          <div className="row justify-content-center">
+            <div className="col-lg align-center" align="center">
+            <Calendar 
+              defaultDate={new Date()}
+              defaultView="month"
+              events={this.state.events}
+              style={{ height: "80vh" }}/>
+            </div>
+          </div>
+        </div>  
+      );
+  }
 
 }

c9929a27f03a41944c0e177a2652cb0996004834
amysudarat <amy.sudarat@gmail.com>
2018-08-13 13:43:30 -0400

    post emotion and redirect to calendarPage from SelectionPage

--- src/Pages/CalendarPageComp.js
+++ src/Pages/CalendarPageComp.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import Calendar from "react-big-calendar";
 import moment from "moment";
 import EmojiComponent from "../Components/EmojiComponent.js";
+import $ from 'jquery';
 
 import "react-big-calendar/lib/css/react-big-calendar.css";
 
@@ -13,16 +14,16 @@ const data = [
     end: "2018-08-01T15:36:27.906Z", 
     title: <EmojiComponent emotion="VerySad"/>
   },
-  {
-    start: "2018-08-03T15:36:27.906Z",
-    end: "2018-08-03T15:36:27.906Z", 
-    title: <EmojiComponent emotion="VeryHappy"/>
-  },
-  {
-    start: new Date(),
-    end: new Date(),
-    title: <EmojiComponent emotion="VerySad"/>
-  },
+  // {
+  //   start: "2018-08-03T15:36:27.906Z",
+  //   end: "2018-08-03T15:36:27.906Z", 
+  //   title: <EmojiComponent emotion="VeryHappy"/>
+  // },
+  // {
+  //   start: new Date(),
+  //   end: new Date(),
+  //   title: <EmojiComponent emotion="VerySad"/>
+  // },
 ]
 
 
@@ -30,6 +31,26 @@ export default class CalendarPageComp extends Component {
   state = {
       events: data
   };
+
+  
+
+  // componentDidMount(){
+  //   console.log("DidMount");
+  //   // let filter = {"where": {"userId": localStorage.getItem("userId")}};
+  //   // let url = "http://localhost:3001/api/emotions?filter="+ JSON.stringify(filter);
+
+
+  //   let filter = {"where": {"userId": localStorage.getItem("userId")}};
+  //   console.log(JSON.stringify(filter))
+    
+  //   let url = "http://localhost:3001/api/emotions"
+    
+  //   $.get(url ).done( (response)=>{
+  //     console.log("getRespond");
+  //     console.log(response);
+  //   }).fail((err)=>{console.log(err);})
+
+  // }
 
   render() {
       return (
--- src/Pages/SelectionPageComp.js
+++ src/Pages/SelectionPageComp.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import {Redirect} from 'react-router-dom'
 import EmojiComponent from '../Components/EmojiComponent.js';
+import $ from 'jquery';
 
 
 export default class SelectionPageComp extends Component {
@@ -7,60 +9,83 @@ export default class SelectionPageComp extends Component {
   constructor(props){
     super(props);
     this.state = {
-      currentEmotion: "Choose a Mood"
+      currentEmotion: "Choose a Mood",
+      redirect: false
     }
   }
 
   update = (e, emo)=>{
-      this.setState({currentEmotion: emo});
+    this.setState({currentEmotion: emo});
+  }
+
+  postEmotion = (event)=>{
+    let data = {
+      "date": "2018-08-03T15:36:27.906Z",
+      "emotion":this.state.currentEmotion,
+      "userId": localStorage.getItem("userId"), 
+    }
+
+    let url = "http://localhost:3001/api/emotions?access_token="+localStorage.getItem("token")
+
+    console.log(data);
+
+    $.post(url, data).done ((response)=>{ 
+      console.log(response)  
+      this.setState({redirect:true});
+    }).fail((err)=> {console.log(err);})
+    
   }
 
   render() {
-    return (
-      <div className="container">
+    if(this.state.redirect) {return <Redirect to='/calendar'/>}
+    else{return (
         <div className="container">
-          <div className="row my-5">
-            <div className="col-lg">
-              <h3>Select your Mood</h3>
+          <div className="container">
+            <div className="row my-5">
+              <div className="col-lg">
+                <h3>Select your Mood</h3>
+              </div>
+            </div>
+            <div className="row my-5">
+              <div className="col-sm">
+                <EmojiComponent alt="Very Happy" emotion="VeryHappy" onClick={(e)=>this.update(e,"VeryHappy")} />
+              </div>
+              <div className="col-sm">
+                <EmojiComponent alt="Happy" emotion="Happy" onClick={(e)=>this.update(e,"Happy")} />
+              </div>
+              <div className="col-sm">
+                <EmojiComponent alt="Neutral" emotion="Neutral" onClick={(e)=>this.update(e,"Neutral")} />
+              </div>
+              <div className="col-sm">              
+                <EmojiComponent alt="Sad" emotion="Sad" onClick={(e)=>this.update(e,"Sad")}/>
+              </div>
+              <div className="col-sm">              
+                <EmojiComponent alt="Very Sad" emotion="VerySad" onClick={(e)=>this.update(e,"VerySad")}/>
+              </div>
+              <div className="col-sm">              
+                <EmojiComponent alt="Angry" emotion="Angry" onClick={(e)=>this.update(e,"Angry")}/>
+              </div>
             </div>
           </div>
-          <div className="row my-5">
-            <div className="col-sm">
-              <EmojiComponent alt="Very Happy" emotion="VeryHappy" onClick={(e)=>this.update(e,"VeryHappy")} />
+  
+          <div className = "container">
+            <div className="row my-5 d-flex align-item-center">
+              <div className="col-lg">
+                <EmojiComponent 
+                  emotion={this.state.currentEmotion}
+                  size = {100} />
+              </div>
             </div>
-            <div className="col-sm">
-              <EmojiComponent alt="Happy" emotion="Happy" onClick={(e)=>this.update(e,"Happy")} />
-            </div>
-            <div className="col-sm">
-              <EmojiComponent alt="Neutral" emotion="Neutral" onClick={(e)=>this.update(e,"Neutral")} />
-            </div>
-            <div className="col-sm">              
-              <EmojiComponent alt="Sad" emotion="Sad" onClick={(e)=>this.update(e,"Sad")}/>
-            </div>
-            <div className="col-sm">              
-              <EmojiComponent alt="Very Sad" emotion="VerySad" onClick={(e)=>this.update(e,"VerySad")}/>
-            </div>
-            <div className="col-sm">              
-              <EmojiComponent alt="Angry" emotion="Angry" onClick={(e)=>this.update(e,"Angry")}/>
+            <div className="row my-5 d-flex align-item-center">
+              <div className="col-lg">
+                <button type="button" className="btn btn-primary" onClick={this.postEmotion}>Set Mood</button>
+              </div>
             </div>
           </div>
         </div>
-
-        <div className = "container">
-          <div className="row my-5 d-flex align-item-center">
-            <div className="col-lg">
-              <EmojiComponent 
-                emotion={this.state.currentEmotion}
-                size = {100} />
-            </div>
-          </div>
-          <div className="row my-5 d-flex align-item-center">
-            <div className="col-lg">
-              <button type="button" className="btn btn-primary">Set Mood</button>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
+      );
+      
+    }
+    
   }
 }
